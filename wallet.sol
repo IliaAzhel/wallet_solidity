b4ea7dd1cb4f80199c38db0cc9c0f54a236a8769
Ilya Azhel <31766003+IliaAzhel@users.noreply.github.com>
2021-07-04 19:05:40 +0300

    Add token ERC20.

--- wallet.sol
+++ wallet.sol
@@ -28,7 +28,51 @@ contract owned{
     }
 }
 
-contract Wallet is owned{
+contract MyToken{
+    string public constant name = "MyToken";
+    string public constant symbol = "MT";
+    uint8 public constant decimals = 2;
+    
+    uint public totalSupply;
+    
+    mapping (address => uint) balances;
+    
+    mapping (address => mapping(address => uint)) allowed;
+    
+    function mint(address _to, uint _value) public{
+        require(totalSupply + _value >= totalSupply && balances[_to] + _value >= balances[_to]);
+        balances[_to] += _value;
+        totalSupply += _value;
+    }
+    
+    function balanceOf(address _owner) public view returns(uint){
+        return balances[_owner];
+    }
+    
+    function allowance(address _owner, address _spender) public view returns(uint){
+        return allowed[_owner][_spender];
+    }
+    
+    function transfer(address _to, uint _value) public{
+        require(balances[msg.sender] >= _value && balances[_to] + _value >= balances[_to]);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+    }
+    
+    function transferFrom(address _from, address _to, uint _value) public payable{
+        require(balances[_from] >= _value && balances[_to] + _value >= balances[_to] && allowed[_from][msg.sender] >= _value);
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        allowed[_from][msg.sender] -= _value;
+    }
+    
+    
+    function approve(address _spender, uint _value) public {
+        allowed[msg.sender][_spender] = _value;
+    }
+}
+
+contract Wallet is owned, MyToken{
     
     uint public comission = 50;
     address payable comissionAddress = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
@@ -46,7 +90,7 @@ contract Wallet is owned{
     }
     
     function changeComission(uint _comission) onlyOwner public{
+        require(_comission < 100);
         comission = _comission;
-        assert(_comission < 100);
     }
 }
